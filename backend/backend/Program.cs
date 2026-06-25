5bfca04da8f7586da52aff5f186b0ef3d6c8f73f
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-06-25 23:31:59 +0200

    Add logging

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,6 +1,22 @@
+using Asp.Versioning;
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 14)
+    .CreateLogger();
+
+Log.Information("Starting API...");
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSerilog();
 
 builder.Services.AddControllers();
 

238665824f8f229798d33c4174021b14ddc20f14
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-07-04 00:59:58 +0200

    #3 - add dtos

---
+++ backend/backend/Models/DTOs/Requests/AvailabilityOverrideRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Models.DTOs.Requests
+{
+    public record AvailabilityOverrideRequest
+    {
+        public DateOnly OverrideDate { get; init; }
+        public bool IsBlocked { get; init; }
+        public TimeOnly? StartTime { get; init; }
+        public TimeOnly? EndTime { get; init; }
+    }
+}
---
+++ backend/backend/Models/DTOs/Responses/OverridesResponse.cs
@@ -0,0 +1,9 @@
+﻿using backend.Models.Entities;
+
+namespace backend.Models.DTOs.Responses
+{
+    public record OverridesResponse
+    {
+        public IEnumerable<AvailabilityOverride>? AvailabilityOverrides { get; init; }
+    }
+}

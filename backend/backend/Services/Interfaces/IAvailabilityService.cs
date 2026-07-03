ca58dc023f364546df072eb78458c495cefa33fe
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-07-03 02:51:00 +0200

    #2 - implement availability service layer

---
+++ backend/backend/Services/Interfaces/IAvailabilityService.cs
@@ -0,0 +1,13 @@
+﻿using backend.Models.DTOs.Requests;
+using backend.Models.DTOs.Responses;
+
+namespace backend.Services.Interfaces
+{
+    public interface IAvailabilityService
+    {
+        Task<AvailabilityResponse> GetAsync(Guid userId);
+        Task<AvailabilityRuleResponse> CreateAsync(Guid userId, AvailabilityRequest request);
+        Task<AvailabilityRuleResponse> UpdateAsync(Guid userId, Guid availabilityRuleId, AvailabilityRequest request);
+        Task DeleteAsync(Guid userId, Guid availabilityRuleId);
+    }
+}

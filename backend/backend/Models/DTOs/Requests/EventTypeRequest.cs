c43ff75a747b58cec475a0f062447e693ca397ab
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-07-05 22:28:35 +0200

    #4 - add eventtype dtos

---
+++ backend/backend/Models/DTOs/Requests/EventTypeRequest.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.DTOs.Requests
+{
+    public record EventTypeRequest
+    {
+        [MaxLength(100)]
+        public required string Title { get; init; }
+
+        [MaxLength(50)]
+        public required string Slug { get; init; }
+
+        [Range(15, 720)]
+        public int DurationMinutes { get; init; }
+
+        [MaxLength(100)]
+        public required string LocationType { get; init; }
+
+        [MaxLength(250)]
+        public required string LocationValue { get; init; }
+
+        [Range(0, 240)]
+        public int BufferBeforeMinutes { get; init; }
+
+        [Range(0, 240)]
+        public int BufferAfterMinutes { get; init; }
+
+        [Range(1, 365)]
+        public int MaxDaysInAdvance { get; init; }
+        public bool IsActive { get; init; }
+    }
+}
---
+++ backend/backend/Models/DTOs/Responses/EventTypeResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace backend.Models.DTOs.Responses
+{
+    public record EventTypeResponse
+    {
+        public Guid Id { get; init; }
+        public required string Title { get; init; }
+        public required string Slug { get; init; }
+        public int DurationMinutes { get; init; }
+        public required string LocationType { get; init; }
+        public required string LocationValue { get; init; }
+        public int BufferBeforeMinutes { get; init; }
+        public int BufferAfterMinutes { get; init; }
+        public int MaxDaysInAdvance { get; init; }
+        public bool IsActive { get; init; }
+        public DateTime CreatedAt { get; init; }
+    }
+}

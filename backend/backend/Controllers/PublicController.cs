9d5ead03af4eebbb743e4094a8123dfd2100bb61
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-07-08 23:38:23 +0200

    #21 - implement endpoint to get available dates

---
+++ backend/backend/Controllers/PublicController.cs
@@ -0,0 +1,25 @@
+﻿using backend.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PublicController : ControllerBase
+    {
+        private readonly ISlotService _slotService;
+
+        public PublicController(ISlotService slotService)
+        {
+            _slotService = slotService;
+        }
+
+        [HttpGet("{userSlug}/{eventTypeSlug}/days")]
+        public async Task<IActionResult> GetAvailableDatesForMonth([FromRoute] string userSlug, [FromRoute] string eventTypeSlug, [FromQuery] int year, [FromQuery] int month)
+        {
+            var availableDates = await _slotService.GetAvailableDatesForMonthAsync(userSlug, eventTypeSlug, year, month);
+
+            return Ok(availableDates);
+        }
+    }
+}

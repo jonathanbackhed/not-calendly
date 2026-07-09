b032ed67148f5c89dcbd9ac4c24fa7092bcfb460
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-07-09 22:07:45 +0200

    #21 - add slot dto

---
+++ backend/backend/Models/DTOs/Responses/SlotResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Models.DTOs.Responses
+{
+    public record SlotResponse
+    {
+        public DateTime StartsAt { get; init; }
+        public DateTime EndsAt { get; init; }
+    }
+}

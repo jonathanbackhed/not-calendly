0bb6856ef5209dedc7d2930307518ed6b1c50d90
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-07-10 00:06:31 +0200

    #23 - implement endpoints

--- backend/backend/Controllers/PublicController.cs
+++ backend/backend/Controllers/PublicController.cs
@@ -8,10 +8,12 @@ namespace backend.Controllers
     public class PublicController : ControllerBase
     {
         private readonly ISlotService _slotService;
+        private readonly IReservationService _reservationService;
 
-        public PublicController(ISlotService slotService)
+        public PublicController(ISlotService slotService, IReservationService reservationService)
         {
             _slotService = slotService;
+            _reservationService = reservationService;
         }
 
         [HttpGet("{userSlug}/{eventTypeSlug}/availabledays")]
@@ -28,6 +30,31 @@ namespace backend.Controllers
             var availableSlots = await _slotService.GetAvailableSlotsForDateAsync(userSlug, eventTypeSlug, date);
 
             return Ok(availableSlots);
+        }
+
+        [HttpPost("{userSlug}/{eventTypeSlug}/reserveslot")]
+        public async Task<IActionResult> ReserveSlot([FromRoute] string userSlug, [FromRoute] string eventTypeSlug, [FromQuery] DateTime startsAt)
+        {
+            var token = await _reservationService.AddReservationAsync(userSlug, eventTypeSlug, startsAt);
+            Response.Cookies.Append("reservation_token", token, new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = DateTimeOffset.UtcNow.AddMinutes(5)
+            });
+
+            return Ok();
+        }
+
+        [HttpDelete("{userSlug}/{eventTypeSlug}/releaseslot")]
+        public async Task<IActionResult> ReleaseSlot([FromRoute] string userSlug, [FromRoute] string eventTypeSlug, [FromQuery] DateTime startsAt)
+        {
+            var reservationToken = Request.Cookies.TryGetValue("reservation_token", out var token) ? token : null;
+            if (reservationToken is null)
+                return BadRequest("Reservation token missing.");
+
+            await _reservationService.RemoveReservationAsync(userSlug, eventTypeSlug, startsAt, reservationToken);
+
+            return Ok();
         }
     }
 }

8d30cae7f842f28d06e8221fb6d1726eea3dee6e
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-07-17 00:28:47 +0200

    #6 - add booking endpoint

--- backend/backend/Controllers/PublicController.cs
+++ backend/backend/Controllers/PublicController.cs
@@ -1,4 +1,5 @@
-﻿using backend.Services.Interfaces;
+﻿using backend.Models.DTOs.Requests;
+using backend.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
@@ -10,12 +11,14 @@ namespace backend.Controllers
         private readonly ISlotService _slotService;
         private readonly IReservationService _reservationService;
         private readonly IEventTypeService _eventTypeService;
+        private readonly IBookingService _bookingService;
 
-        public PublicController(ISlotService slotService, IReservationService reservationService, IEventTypeService eventTypeService)
+        public PublicController(ISlotService slotService, IReservationService reservationService, IEventTypeService eventTypeService, IBookingService bookingService)
         {
             _slotService = slotService;
             _reservationService = reservationService;
             _eventTypeService = eventTypeService;
+            _bookingService = bookingService;
         }
 
         // AVAILABILITY
@@ -69,6 +72,31 @@ namespace backend.Controllers
             Response.Cookies.Delete("reservation_token");
 
             return Ok();
+        }
+
+        // BOOKINGS
+        [HttpPost("{userSlug}/{eventTypeSlug}/bookings")]
+        public async Task<IActionResult> BookSlot([FromBody] BookingRequest request, [FromRoute] string userSlug, [FromRoute] string eventTypeSlug, [FromQuery] DateTime startsAt)
+        {
+            var reservationToken = Request.Cookies.TryGetValue("reservation_token", out var token) ? token : "";
+
+            var slotAvailable = await _slotService.IsSlotAvailableAsync(userSlug, eventTypeSlug, startsAt, reservationToken);
+            if (!slotAvailable)
+            {
+                return BadRequest();
+            }
+
+            var booking = await _bookingService.CreateBooking(request, userSlug, eventTypeSlug, startsAt);
+
+            if (!string.IsNullOrEmpty(reservationToken))
+            {
+                await _reservationService.RemoveReservationAsync(userSlug, eventTypeSlug, startsAt, reservationToken);
+                Response.Cookies.Delete("reservation_token");
+            }
+
+            // Send confirmation email to host & user
+
+            return Ok(booking);
         }
     }
 }

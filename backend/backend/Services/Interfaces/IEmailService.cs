a0c6c64d268d65f87dcbffc8aad007055dd1ac35
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-07-18 01:23:00 +0200

    #5 - implement email service

---
+++ backend/backend/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using backend.Models.DTOs.Responses;
+
+namespace backend.Services.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendConfirmationEmail(BookingResponse bookingResponse);
+        Task SendCancellationEmail(string cancelToken);
+    }
+}

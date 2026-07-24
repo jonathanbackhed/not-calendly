281f248cd59ce3d81fe8dde8b078aa8a07bde1aa
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-07-25 00:56:32 +0200

    #35 - update back logic in modal

--- web/src/components/ui/ModalSkeleton.tsx
+++ web/src/components/ui/ModalSkeleton.tsx
@@ -11,12 +11,23 @@ interface Props {
 export default function ModalSkeleton({ children }: Props) {
   const router = useRouter();
 
+  const handleClose = () => {
+    const authRoutes = ["/login", "/register", "/forgot-password"];
+    const prevUrl = document.referrer;
+
+    if (authRoutes.some((route) => prevUrl.includes(route))) {
+      router.push("/");
+    } else {
+      router.back();
+    }
+  };
+
   return (
     <div className="absolute top-0 right-0 bottom-0 left-0 z-40 flex h-screen w-screen items-center justify-center bg-black/50">
       <div className="bg-background rounded-lg p-4">
         <div className="flex justify-end">
           <button
-            onClick={() => router.back()}
+            onClick={handleClose}
             className="rounded-full bg-white p-1 shadow-xs hover:cursor-pointer hover:opacity-80"
           >
             <X size={24} color="black" />

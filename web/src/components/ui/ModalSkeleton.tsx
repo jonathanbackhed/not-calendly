d6aa0d1e8a40b787586e652624eb7f440a48ebc2
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-07-25 01:58:53 +0200

    Update ModalSkeleton.tsx

--- web/src/components/ui/ModalSkeleton.tsx
+++ web/src/components/ui/ModalSkeleton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { X } from "lucide-react";
+import { ArrowLeft, X } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React from "react";
 
@@ -11,26 +11,16 @@ interface Props {
 export default function ModalSkeleton({ children }: Props) {
   const router = useRouter();
 
-  const handleClose = () => {
-    const authRoutes = ["/login", "/register", "/forgot-password"];
-    const prevUrl = document.referrer;
-
-    if (authRoutes.some((route) => prevUrl.includes(route))) {
-      router.push("/");
-    } else {
-      router.back();
-    }
-  };
-
   return (
     <div className="absolute top-0 right-0 bottom-0 left-0 z-40 flex h-screen w-screen items-center justify-center bg-black/50">
       <div className="bg-background rounded-lg p-4">
-        <div className="flex justify-end">
+        <div className="flex justify-start">
           <button
-            onClick={handleClose}
+            onClick={() => router.back()}
             className="rounded-full bg-white p-1 shadow-xs hover:cursor-pointer hover:opacity-80"
           >
-            <X size={24} color="black" />
+            {/* <X size={24} color="black" /> */}
+            <ArrowLeft size={24} color="black" />
           </button>
         </div>
         {children}

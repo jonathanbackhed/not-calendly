cef6e4fefdf56a3d7e48f437139e860184bba786
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-07-26 01:30:12 +0200

    Update eslint.config.mjs

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -13,6 +13,12 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;

0cabfba22daa6f062cba23843d5ed7be3aefc86b
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-07-27 14:11:53 +0200

    #41 - add hero section

--- web/src/app/(public)/(with-navbar)/page.tsx
+++ web/src/app/(public)/(with-navbar)/page.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex flex-1 items-center justify-center">
-      <h1 className="text-4xl">Hello world!</h1>
+    <div className="flex flex-col">
+      <div className="flex flex-1 flex-col items-center justify-center space-y-6 bg-red-100 p-48">
+        <h1 className="font-special text-center text-7xl font-light">
+          Let clients book you. <br /> You focus on the work.
+        </h1>
+        <p className="text-center">
+          Share your booking link and let clients schedule time with you - no back-and-forth, no hassle.
+        </p>
+        <Link href={"#"} className="rounded-full bg-black px-8 py-4 text-white shadow-xs">
+          How it works
+        </Link>
+      </div>
     </div>
   );
 }
--- web/src/app/(public)/layout.tsx
+++ web/src/app/(public)/layout.tsx
@@ -1,6 +1,6 @@
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto flex min-h-screen w-full max-w-7xl flex-col border-x border-neutral-200">
+    <div className="mx-auto flex min-h-screen w-full max-w-6xl flex-col border-x border-neutral-200">
       <div className="h-10 border-b border-neutral-200" />
       {children}
       <div className="static bottom-0 left-0 h-10 w-full border-t border-neutral-200" />
